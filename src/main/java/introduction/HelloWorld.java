8baca72bdbaabcd07fbc7fb0708860bcd646751a
miljab <jablonskimilosz1337@gmail.com>
2021-10-04 14:50:35 +0200

    Kod wynikowy

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,14 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+    static Logger log = Logger.getLogger("name");
+
     public static void main(String [ ] args) {
+
+        log.info("message");
         System.out.println("Hello! Witaj!");
     }
 }
